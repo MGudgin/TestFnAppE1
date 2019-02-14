08dd11ace235764e4ece558f23077ff6a43d7f9b
Martin Gudgin <mgudgin@microsoft.com>
2019-02-14 11:43:31 -0800

    Add a function that throws an exception
    Add an Azure Function that does some logging and then
    throws an exception before it completes. This is for
    testing what we actaully get back on the caller side.

---
+++ LevelCompleteDualBindingGenericCrashFn.cs
@@ -0,0 +1,35 @@
+namespace PlayFab.CloudScript
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Logging;
+
+    public static class LevelCompleteDualBindingGenericCrashFn
+    {
+        [FunctionName(nameof(LevelCompleteDualBindingGenericCrashFn))]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] EntityRequest<LevelComplete> req,
+            HttpRequest httpRequest,
+            ILogger log)
+        {
+            log.LogInformation($"{nameof(LevelCompleteDualBindingGenericFn)} processed a request.");
+
+            log.LogInformation($"Level: {req.FunctionParameter.level} Points: {req.FunctionParameter.points}");
+
+            log.LogInformation($"HTTPS: {httpRequest.IsHttps}");
+
+            await Task.Delay(50); // Simulate some async work
+
+            // Crash
+            string crash = null;
+            log.LogInformation($"String is {crash.Length} characters");
+
+            return (ActionResult)new OkObjectResult($"Congrats on completing level {req.FunctionParameter.level}");
+        }
+    }
+}
