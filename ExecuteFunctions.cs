94378946d4c6ce1a471bd6a35e3a48d856a3ea58
Martin Gudgin <mgudgin@microsoft.com>
2019-03-10 07:21:01 -0700

    Use null route on ExecuteFunction
    Turns out routes cannot begin with /

--- ExecuteFunctions.cs
+++ ExecuteFunctions.cs
@@ -1,31 +1,38 @@
 namespace PlayFab.CloudScript
 {
     using System;
+    using System.Diagnostics;
     using System.IO;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
     using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.Azure.WebJobs;
     using Microsoft.Azure.WebJobs.Extensions.Http;
-    using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.Logging;
 
-    public static class LevelCompleteDualBindingFn
+    public static class ExecuteFunction
     {
-        [FunctionName(nameof(LevelCompleteDualBindingFn))]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] LevelCompleteRequest req,
-            HttpRequest httpRequest,
-            ILogger log)
+        /// <summary>
+        /// A local implementation of the ExecuteFunction feature. Provides the ability to execute an Azure Function with a local URL with respect to the host
+        /// of the application this function is running in.
+        /// </summary>
+        /// <param name="httpRequest">The HTTP request</param>
+        /// <param name="log">A logger object</param>
+        /// <returns>The function execution result(s)</returns>
+        [FunctionName("ExecuteFunction")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest request, ILogger log)
         {
-            log.LogInformation($"{nameof(LevelCompleteDualBindingFn)} processed a request.");
+            await Task.Delay(100);
 
-            log.LogInformation($"Level: {req.FunctionParameter.level} Points: {req.FunctionParameter.points}");
-
-            log.LogInformation($"HTTPS: {httpRequest.IsHttps}");
-
-            await Task.Delay(50); // Simulate some async work
-
-            return (ActionResult)new OkObjectResult($"Congrats on completing level {req.FunctionParameter.level}");
+            return new HttpResponseMessage
+            {
+                Content = new StringContent("{}", Encoding.UTF8, "application/json"),
+                StatusCode = HttpStatusCode.OK
+            };
         }
     }
 }
